95f22d796112af0b6f38eafa8efa4005fe847f65
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-25 16:37:41 +0100

    created database class to connect and run queries with

---
+++ core/database.php
@@ -0,0 +1,44 @@
+<?php
+
+class DB
+{
+    protected static $instance;
+    protected $pdo;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHAR;
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+        try {
+            $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function instance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->pdo, $method), $args);
+    }
+
+    public function run($sql, $args = [])
+    {
+        if (!$args) {
+            return $this->query($sql);
+        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+}
