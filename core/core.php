1f31dd3b1e9090b4140ada1cbfa5f9dd908515e7
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 14:42:55 +0100

    added docbloc to view method

--- core/core.php
+++ core/core.php
@@ -1,6 +1,13 @@
 <?php
 class Core
 {
+    /**
+     * display the requested view with the needed data
+     *
+     * @param [type] $file
+     * @param [type] $data
+     * @return view
+     */
     public static function view($file, $data = null)
     {
         if ($data) {
