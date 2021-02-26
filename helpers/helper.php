f460b9f7a82a22e2e00cafb8ac7fb8b65b5b405a
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 13:45:04 +0100

    find() will return false it it couldn't find anything and added docbloc

--- helpers/helper.php
+++ helpers/helper.php
@@ -1,6 +1,15 @@
 <?php
 class Helper
 {
+    /**
+     * find specific object in object array
+     * returns false if it couldn't find anything
+     *
+     * @param [type] $array
+     * @param [type] $searchKey
+     * @param [type] $searchValue
+     * @return object
+     */
     public static function find($array, $searchKey, $searchValue)
     {
         foreach ($array as $sub) {
@@ -8,5 +17,6 @@ class Helper
                 return $sub;
             }
         }
+        return false;
     }
 }
