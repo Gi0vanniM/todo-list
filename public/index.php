71365e07e9e4aa4950fe4429fa245efa79ae1410
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 14:42:15 +0100

    require core.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-echo 'index.php';
-
 define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 
 require(ROOT . 'config.php');
+require(ROOT . 'core/core.php');
 require(ROOT . 'core/route.php');
 require(ROOT . 'core/request.php');
 require(ROOT . 'helpers/helper.php');
