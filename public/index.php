410c96be20b48258cd1d0b6012d2708b1d5d0d89
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 13:36:57 +0100

    require helper file

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require(ROOT . 'config.php');
 require(ROOT . 'core/route.php');
 require(ROOT . 'core/request.php');
+require(ROOT . 'helpers/helper.php');
 
 $route = new Route();
 $route->route();
