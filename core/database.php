67f2331aeca46c65d66d3c6453e1a45f1af7914d
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 13:21:26 +0100

    changed default fetchmode to object

--- core/database.php
+++ core/database.php
@@ -1,5 +1,4 @@
 <?php
-
 class DB
 {
     protected static $instance;
@@ -10,7 +9,7 @@ class DB
         $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHAR;
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         ];
         try {
             $this->pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
