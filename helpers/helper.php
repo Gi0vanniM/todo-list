0abd726f654a9e363dbd0b3559970c728dd9739e
Gi0vanniM <g.l.mierop@gmail.com>
2021-02-26 13:36:20 +0100

    made a find function which will return the first object it matches

--- helpers/helper.php
+++ helpers/helper.php
@@ -1,4 +1,12 @@
 <?php
 class Helper
 {
+    public static function find($array, $searchKey, $searchValue)
+    {
+        foreach ($array as $sub) {
+            if ($sub->$searchKey === $searchValue) {
+                return $sub;
+            }
+        }
+    }
 }
