8335454a30d56b136b099f3305574cd4763d2f0c
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-05 12:07:23 +0100

    start_session()

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require(ROOT . 'config.php');
 require(ROOT . 'Helpers/autoload.php');
 
+// start session if there isn't one
+if (!isset($_SESSION)) {
+    session_start();
+}
+
 $route = new Core\Route();
 $route->route();
