0431cc1d7343b8deada09c3bea015b88cbc6cbfd
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-05 10:04:17 +0100

    made register form functional

--- views/auth/register.php
+++ views/auth/register.php
@@ -6,34 +6,35 @@
 
         <div class="col-lg-10 col-xl-9 mx-auto">
             <div class="card card-signin flex-row my-5">
-    
+
                 <div class="card-body">
                     <h5 class="card-title text-center">Register</h5>
-                    <form class="form-signin">
+                    <form action="register/register" method="post" class="form-signin">
+
                         <div class="form-label-group">
-                            <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
-                            <label for="inputUserame">Username</label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+                            <label for="username">Username</label>
                         </div>
 
                         <div class="form-label-group">
-                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                            <label for="inputEmail">Email address</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Email address" required>
+                            <label for="email">Email address</label>
                         </div>
-            
+
                         <hr>
 
                         <div class="form-label-group">
-                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                            <label for="inputPassword">Password</label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+                            <label for="password">Password</label>
                         </div>
-            
+
                         <div class="form-label-group">
-                            <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Password" required>
-                            <label for="inputConfirmPassword">Confirm password</label>
+                            <input type="password" name="confirmpassword" id="confirmpassword" class="form-control" placeholder="Password" required>
+                            <label for="confirmpassword">Confirm password</label>
                         </div>
 
                         <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Register</button>
-                        <a class="d-block text-center mt-2 small" href="#">Sign In</a>
+                        <a class="d-block text-center mt-2 small" href="login">Sign In</a>
                         <hr class="my-4">
                     </form>
                 </div>
