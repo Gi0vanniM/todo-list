c9ec54d7dbab0c160c0e4dd0117c7e487380d686
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-08 01:08:21 +0100

    list template

---
+++ views/board/index.php
@@ -0,0 +1,58 @@
+<?php 
+
+?>
+
+<div class="list-container px-2 mt-3">
+
+
+    <div class="list">
+        <h3 class="list-title">List title</h3>
+        <ul class="list-items">
+            <li>Task 1</li>
+            <li>Task 2</li>
+            <li>Task 3</li>
+            <li>Task 4</li>
+            <li>Task 5</li>
+            <li>Task 6</li>
+            <li>Task 7</li>
+            <li>Task 8</li>
+            <li>Task 9</li>
+            <li>Task 10</li>
+            <li>Task 11</li>
+            <li>Task 12</li>
+            <li>Task 13</li>
+            <li>Task 14</li>
+            <li>Task 15</li>
+            <li>Task 16</li>
+            <li>Task 17</li>
+            <li>Task 18</li>
+            <li>Task 19</li>
+            <li>Task 20</li>
+            <li>Task 21</li>
+            <li>Task 22</li>
+            <li>Task 23</li>
+            <li>Task 24</li>
+            <li>Task 25</li>
+            <li>Task 26</li>
+            <li>Task 27</li>
+            <li>Task 28</li>
+            <li>Task 29</li>
+            <li>Task 30</li>
+            <li>Task 31</li>
+            <li>Task 32</li>
+            <li>Task 33</li>
+            <li>Task 34</li>
+            <li>Task 35</li>
+            <li>Task 36</li>
+            <li>Task 37</li>
+            <li>Task 38</li>
+            <li>Task 39</li>
+            <li>Task 40</li>
+        </ul>
+        <button class="btn add-card-btn">Add a card</button>
+    </div>
+
+    <button class="btn add-list-btn">Add a list</button>
+
+
+</div>
