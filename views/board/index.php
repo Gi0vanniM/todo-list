7476913fa86051d4df6c8ac80ba253c1f8c3bcd2
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-08 14:43:04 +0100

    made header title textarea and added the three dots button

--- views/board/index.php
+++ views/board/index.php
@@ -4,53 +4,26 @@
 
 <div class="list-container px-2 mt-3">
 
+    <!-- <template class="list-template"> -->
 
-    <div class="list">
-        <h3 class="list-title">List title</h3>
-        <ul class="list-items">
-            <li>Task 1</li>
-            <li>Task 2</li>
-            <li>Task 3</li>
-            <li>Task 4</li>
-            <li>Task 5</li>
-            <li>Task 6</li>
-            <li>Task 7</li>
-            <li>Task 8</li>
-            <li>Task 9</li>
-            <li>Task 10</li>
-            <li>Task 11</li>
-            <li>Task 12</li>
-            <li>Task 13</li>
-            <li>Task 14</li>
-            <li>Task 15</li>
-            <li>Task 16</li>
-            <li>Task 17</li>
-            <li>Task 18</li>
-            <li>Task 19</li>
-            <li>Task 20</li>
-            <li>Task 21</li>
-            <li>Task 22</li>
-            <li>Task 23</li>
-            <li>Task 24</li>
-            <li>Task 25</li>
-            <li>Task 26</li>
-            <li>Task 27</li>
-            <li>Task 28</li>
-            <li>Task 29</li>
-            <li>Task 30</li>
-            <li>Task 31</li>
-            <li>Task 32</li>
-            <li>Task 33</li>
-            <li>Task 34</li>
-            <li>Task 35</li>
-            <li>Task 36</li>
-            <li>Task 37</li>
-            <li>Task 38</li>
-            <li>Task 39</li>
-            <li>Task 40</li>
-        </ul>
-        <button class="btn add-card-btn">Add a card</button>
-    </div>
+        <div class="list">
+            <div class="list-header">
+                <textarea class="list-title m-0">List title title title title title title title</textarea>
+
+                <div class="list-header-extra">
+                    <a href="#" class="list-option">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </a>
+                </div>
+
+            </div>
+            <ul class="list-items m-0">
+                <!-- <li></li> -->
+            </ul>
+            <button class="btn add-card-btn">Add a card</button>
+        </div>
+
+    <!-- </template> -->
 
     <button class="btn add-list-btn">Add a list</button>
 
