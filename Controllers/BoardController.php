dde9004a02694f2daf14c4ac7a4ac09cdfb54e8e
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-10 12:55:43 +0100

    /board/addList routes you back. start off handling list creation

--- Controllers/BoardController.php
+++ Controllers/BoardController.php
@@ -1,10 +1,27 @@
-<?php namespace Controllers;
+<?php
+
+namespace Controllers;
 
 use Core\Core;
+use Helpers\Helper;
 
 class BoardController 
 {
+    public static $boardUrl = "location: //" . APP_URL . "/board";
+
     public function index() {
         return Core::view("board/index", ['title' => 'Board']);
+    }
+
+    public function addList()
+    {
+        if (!Helper::isPostSet('addList')) {
+            return header(self::$boardUrl);
+        }
+        // TODO: add/create list
+
+
+
+        return header(self::$boardUrl);
     }
 }
