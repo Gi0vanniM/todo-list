92593d77ba9e29173624abcb6f7f5edc8be5e5e2
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-15 12:49:00 +0100

    add task dropdown form. duration input and status

--- views/board/index.php
+++ views/board/index.php
@@ -18,8 +18,12 @@
                             <i class="fas fa-ellipsis-h"></i>
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownListOptions<?= $list->id ?>">
-                            <li><h6 class="dropdown-header">List actions</h6></li>
-                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <h6 class="dropdown-header">List actions</h6>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                             <li>
                                 <form action="//<?= APP_URL ?>/board/deleteList/<?= $list->id ?>" method="post">
                                     <button name="id" value="<?= $list->id ?>" class="dropdown-item text-danger">Delete</button>
@@ -40,6 +44,21 @@
                 <form action="//<?= APP_URL ?>/board/addTask/<?= $list->id ?>" method="post" class="dropdown-menu addTask bg-greyish p-2 m-0" aria-labelledby="dropdownAddTask<?= $list->id ?>">
                     <div class="form-group m-0">
                         <input type="text" id="newTaskName" name="newTaskName" class="w-100" placeholder="Enter task description">
+
+                        <div>
+                            <input type="number" name="taskDuration" id="taskDuration" class="w-50" min="0" placeholder="Duration">
+                            <label for="taskDuration">minutes</label>
+                        </div>
+
+                        <div>
+                            <label for="taskStatus">Status:</label>
+                            <select name="taskStatus" id="taskStatus" class="w-75">
+                                <?php foreach ($statuses as $status) { ?>
+                                    <option value="<?= $status->id ?>"><?= $status->status ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+
                         <button type="submit" name="addTask" class="btn btn-success py-1 px-2 mt-2">Add task</button>
                     </div>
                 </form>
@@ -62,8 +81,12 @@
                             <i class="fas fa-ellipsis-h"></i>
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownListOptions#">
-                            <li><h6 class="dropdown-header">List actions</h6></li>
-                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <h6 class="dropdown-header">List actions</h6>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                             <li>
                                 <form action="//<?= APP_URL ?>/board/deleteList/#" method="post">
                                     <button name="id" value="#" class="dropdown-item text-danger">Delete</button>
@@ -83,7 +106,8 @@
 
                 <form action="//<?= APP_URL ?>/board/addTask/<?= $list->id ?>" method="post" class="dropdown-menu addTask bg-greyish p-2 m-0" aria-labelledby="dropdownAddTask#">
                     <div class="form-group m-0">
-                        <input type="text" id="newTaskName" name="newTaskName" class="w-100" placeholder="Enter task description">
+                        <input type="text" id="taskDescription" name="taskDescription" class="w-100" placeholder="Enter task description">
+                        <input type="number" name="duration" id="taskDuration">
                         <button type="submit" name="addTask" class="btn btn-success py-1 px-2 mt-2">Add task</button>
                     </div>
                 </form>
