c98c98dbf1b23d5c7f06ce5a9cc77982b525f831
Gi0vanniM <g.l.mierop@gmail.com>
2021-03-16 12:32:04 +0100

    Create task method

--- Model/Task.php
+++ Model/Task.php
@@ -20,4 +20,39 @@ class Task
     {
         $this->db = new Database();
     }
+
+    /**
+     * create a task
+     *
+     * @param int $list_id
+     * @param string $description
+     * @param int $duration
+     * @param int $status_id
+     * @return boolean
+     */
+    public function create($list_id, $description, $duration, $status_id)
+    {
+        if (!$list_id || !$description || !$duration || !$status_id) {
+            return false;
+        }
+
+        $this->list_id = Helper::sanitize($list_id);
+        $this->description = Helper::sanitize($description);
+        $this->duration = Helper::sanitize($duration);
+        $this->status_id = Helper::sanitize($status_id);
+
+        $sql = 'INSERT INTO tasks (list_id, description, duration, status_id) VALUES (:list_id, :description, :duration, :status_id)';
+        $args = [
+            'list_id' => $this->list_id,
+            'description' => $this->description,
+            'duration' => $this->duration,
+            'status_id' => $this->status_id,
+        ];
+
+        if ($this->db->run($sql, $args)) {
+            return true;
+        }
+
+        return false;
+    }
 }
